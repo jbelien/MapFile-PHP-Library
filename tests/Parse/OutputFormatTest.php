3add07cd23b215807d2f3287bcf231bb7a339a44
Jonathan Beliën <dev@jbelien.be>
2022-11-06 19:57:57 +0100

    Create tests for OUTPUTFORMAT

---
+++ tests/Parse/OutputFormatTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Parse;
+
+use MapFile\Parser\OutputFormat;
+use PHPUnit\Framework\TestCase;
+
+final class OutputFormatTest extends TestCase
+{
+    protected string $stub = 'tests/stubs/OUTPUTFORMAT';
+
+    public function test(): void
+    {
+        $parser = new OutputFormat($this->stub);
+        $outputformat = $parser->parse();
+
+        $this->assertSame('AGG/PNG8', $outputformat->driver);
+        $this->assertSame('png', $outputformat->extension);
+        $this->assertSame(['QUANTIZE_FORCE=on', 'QUANTIZE_COLORS=256', 'GAMMA=0.75'], $outputformat->formatoption);
+        $this->assertSame('RGB', $outputformat->imagemode);
+        $this->assertSame('image/png; mode=8bit', $outputformat->mimetype);
+        $this->assertSame('png8', $outputformat->name);
+        $this->assertSame('ON', $outputformat->transparent);
+    }
+}
---
+++ tests/Write/OutputFormatTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Write;
+
+use MapFile\Model\OutputFormat as OutputFormatObject;
+use MapFile\Model\Style;
+use MapFile\Writer\OutputFormat;
+use Tests\WriteTest;
+
+final class OutputFormatTest extends WriteTest
+{
+    protected string $path = 'tests/.temp/OUTPUTFORMAT.test';
+    protected string $stub = 'tests/stubs/OUTPUTFORMAT';
+
+    public function test(): void
+    {
+        $outputformat = new OutputFormatObject();
+        $outputformat->driver = 'AGG/PNG8';
+        $outputformat->extension = 'png';
+        $outputformat->formatoption = [
+            'QUANTIZE_FORCE=on',
+            'QUANTIZE_COLORS=256',
+            'GAMMA=0.75',
+        ];
+        $outputformat->imagemode = 'RGB';
+        $outputformat->mimetype = 'image/png; mode=8bit';
+        $outputformat->name = 'png8';
+        $outputformat->transparent = 'ON';
+
+        $writer = new OutputFormat($this->path);
+        $writer->write($outputformat);
+        $result = $writer->save();
+
+        $this->assertTrue($result);
+        $this->assertFileEquals($this->stub, $this->path);
+    }
+}
