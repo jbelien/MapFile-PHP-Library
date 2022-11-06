abea6350c55e0f8e52b993a76be4f95cb41f7ce5
Jonathan Beliën <dev@jbelien.be>
2022-11-06 18:36:20 +0100

    Update Writer.php

--- src/Writer/Writer.php
+++ src/Writer/Writer.php
@@ -34,7 +34,14 @@ abstract class Writer implements WriterInterface
         return false;
     }
 
-    protected static function getTextRaw(string $key, mixed $value, int $indentSize = 0, string $indent = self::WRITER_INDENT): string
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @param int $indentSize
+     * @param string $indent
+     * @return string
+     */
+    protected static function getTextRaw(string $key, $value, int $indentSize = 0, string $indent = self::WRITER_INDENT): string
     {
         $text = '';
 
