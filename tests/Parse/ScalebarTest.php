4eb269fe33edcc031ef65939e2dc4ced930e108b
Jonathan Beliën <dev@jbelien.be>
2022-11-07 13:47:11 +0100

    Update ScalebarTest.php

--- tests/Parse/ScalebarTest.php
+++ tests/Parse/ScalebarTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Parse;
 
+use MapFile\Model\Label;
 use MapFile\Parser\Scalebar;
 use Tests\ParseTest;
 
@@ -19,7 +20,7 @@ final class ScalebarTest extends ParseTest
         self::assertSame([0, 0, 0], $scalebar->color);
         self::assertSame([0, 255, 0], $scalebar->imagecolor);
         self::assertSame(4, $scalebar->intervals);
-        self::assertNotNull($scalebar->label);
+        self::assertInstanceOf(Label::class, $scalebar->label);
         self::assertSame([80, 80, 80], $scalebar->label->color);
         self::assertSame([5, 5], $scalebar->offset);
         self::assertSame([0, 0, 0], $scalebar->outlinecolor);
