0996c04a681c09f5af72ec9a4c9075327b3b2b71
Jonathan Beliën <dev@jbelien.be>
2022-11-07 12:35:50 +0100

    Create tests for WEB

---
+++ tests/Parse/WebTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Parse;
+
+use MapFile\Parser\Web;
+use Tests\ParseTest;
+
+final class WebTest extends ParseTest
+{
+    public function test(): void
+    {
+        $parser = new Web($this->stub);
+        $web = $parser->parse();
+
+        self::assertSame('text/html', $web->browseformat);
+        self::assertSame('/empty', $web->empty);
+        self::assertSame('/error', $web->error);
+        self::assertSame('footer.html', $web->footer);
+        self::assertSame('header.html', $web->header);
+        self::assertSame('/imagepath/', $web->imagepath);
+        self::assertSame('localhost', $web->imageurl);
+        self::assertSame('text/html', $web->legendformat);
+        self::assertSame(10000.0, $web->maxscaledenom);
+        self::assertSame('maxscale', $web->maxtemplate);
+        self::assertSame(['labelcache_map_edge_buffer' => '10', 'ms_enable_modes' => '!*'], $web->metadata);
+        self::assertSame(1000.0, $web->minscaledenom);
+        self::assertSame('minscale', $web->mintemplate);
+        self::assertSame('text/html', $web->queryformat);
+        self::assertSame('template.html', $web->template);
+        self::assertSame('/temp/', $web->temppath);
+        self::assertSame(['firstname' => '^[a-zA-Z\-]+$'], $web->validation);
+    }
+}
---
+++ tests/Write/WebTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Write;
+
+use MapFile\Model\Web as WebObject;
+use MapFile\Writer\Web;
+use Tests\WriteTest;
+
+final class WebTest extends WriteTest
+{
+    public function test(): void
+    {
+        $web = new WebObject();
+        $web->browseformat = 'text/html';
+        $web->empty = '/empty';
+        $web->error = '/error';
+        $web->footer = 'footer.html';
+        $web->header = 'header.html';
+        $web->imagepath = '/imagepath/';
+        $web->imageurl = 'localhost';
+        $web->legendformat = 'text/html';
+        $web->maxscaledenom = 10000;
+        $web->maxtemplate = 'maxscale';
+        $web->metadata = ['labelcache_map_edge_buffer' => '10', 'ms_enable_modes' => '!*'];
+        $web->minscaledenom = 1000;
+        $web->mintemplate = 'minscale';
+        $web->queryformat = 'text/html';
+        $web->template = 'template.html';
+        $web->temppath = '/temp/';
+        $web->validation = ['firstname' => '^[a-zA-Z\-]+$'];
+
+        $writer = new Web($this->path);
+        $writer->write($web);
+        $result = $writer->save();
+
+        self::assertTrue($result);
+        self::assertFileEquals($this->stub, $this->path);
+    }
+}
